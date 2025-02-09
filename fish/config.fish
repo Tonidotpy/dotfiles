24b10ff81d5227489f762a56d61a7a3d46f11109
Antonio Gelain <antonio.gelain2@gmail.com>
2025-02-09 17:19:22 +0100

    fix: wayland environment variables

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,16 @@
 if status is-login
-    set -gx XDG_CONFIG_HOME "$HOME/.config"
+    # Create XDG runtime directory if non existent
+    if ! set -q XDG_RUNTIME_DIR
+	set -Ux XDG_RUNTIME_DIR /run/user/$(id -u)
+    end
+    if test ! -d "$XDG_RUNTIME_DIR"
+        mkdir "$XDG_RUNTIME_DIR"
+        chmod 0700 "$XDG_RUNTIME_DIR"
+    end
 
+    # Start River Window Manager
     if test -z "$WAYLAND_DISPLAY" -a "$XDG_VTNR" -eq 1
-	    exec river
+        exec river
     end
 end
 
