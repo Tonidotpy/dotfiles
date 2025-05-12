2c485b36bc0402a80e9a966daa3241dfba81032a
Antonio Gelain <antonio.gelain2@gmail.com>
2025-05-12 11:12:05 +0200

    feat: update dotfiles

---
+++ nvim/lua/tonidotpy/plugins/conform.lua
@@ -0,0 +1,32 @@
+return {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
+
+        conform.setup({
+            formatters_by_ft = {
+                c = { "clang_format" },
+                json = { "clang_format" }
+            },
+            -- format_on_save = {
+            --     lsp_fallback = true,
+            --     async = false,
+            --     timeout_ms = 500,
+            -- },
+            formatters = {
+                clang_format = {
+                    prepend_args = { '--style=file', '--fallback-style=LLVM' }
+                }
+            }
+        })
+
+        vim.keymap.set({ "n", "v" }, "<leader>cf", function()
+            conform.format({
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 500,
+            })
+        end, { desc = "Format file or range (in visual mode)" })
+    end,
+}
